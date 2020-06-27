eac6f4876de869197ed24c0411ab94bbb2ea9f94
Jyoti <zyotieebista12@gmail.com>
2020-06-27 13:32:02 +0545

    lec 24 done

--- mysite/articles/views.py
+++ mysite/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Articles
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -12,3 +13,8 @@ def articles_list(request):
 def articles_details(request, slug):
     article = Articles.objects.get(slug=slug)
     return render(request, 'articles/articles_details.html', {'article': article})
+
+
+@login_required(login_url="/accounts/login/")
+def articles_create(request):
+    return render(request, 'articles/articles_create.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^accounts/', include('accounts.urls', namespace="accounts")),
     url(r'^articles/', include('articles.urls', namespace="articles")),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
